583f7fd4e0d1ed7a99e67297a03c47002a7cade1
Alexandra <mirinda-001@list.ru>
2021-05-05 16:39:43 +0300

    123

--- index.js
+++ index.js
@@ -7,31 +7,38 @@ const axios = require('axios');
 try {
   const filepath = core.getInput('filepath');
   const slack_url = core.getInput('slack_url');
-  var data = fs.readFileSync(filepath);
-  parseString(data, (err, res) => {
-    const data = res.TestRun.ResultSummary[0].Counters[0]['$']
-    console.log(data);
 
-    const total = data.total;
-    const passed = data.passed;
-    const failed = data.failed;
+  fs.readdir(filepath, (err, files) => {
+    const path = files[0];
+    
+    var data = fs.readFileSync(filepath + '/' + path);
+    
+    parseString(data, (err, res) => {
+      const data = res.TestRun.ResultSummary[0].Counters[0]['$']
+      console.log(data);
 
-    axios
-    .post(slack_url, {
-      "channel":"#uqrew",
-      "icon_emoji":":serega:",
-      "username":"Uqrew Backend Builder",
-      "text":`Test results:\n Total: ${total}. Passed: ${passed}. Failed: ${failed}`
-    })
-    .then(res => {
-      console.log(`statusCode: ${res.statusCode}`)
-      console.log(res)
-    })
-    .catch(error => {
-      console.error(error)
-    })
+      const total = data.total;
+      const passed = data.passed;
+      const failed = data.failed;
 
-    console.log(total);
+      axios
+        .post(slack_url, {
+          "channel":"#uqrew",
+          "icon_emoji":":serega:",
+          "username":"Uqrew Backend Builder",
+          "text":`Test results:\n Total: ${total}. Passed: ${passed}. Failed: ${failed}`
+        })
+        .then(res => {
+          console.log(`statusCode: ${res.statusCode}`)
+          console.log(res)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+
+        console.log(total);
+    });
+
   });
 
 } catch (error) {
